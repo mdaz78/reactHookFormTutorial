5c64045e1964f9e2f9c5ef72e1a28ce480a8758a
Md Abu Zafar <mdabuzafar78@gmail.com>
2024-04-05 00:38:52 +0530

    learn about - dynamic field - how to use date and numeric type

--- src/components/YouTubeForm.tsx
+++ src/components/YouTubeForm.tsx
@@ -1,24 +1,37 @@
 import { DevTool } from "@hookform/devtools";
-import { useForm } from "react-hook-form";
+import { useFieldArray, useForm } from "react-hook-form";
 
 type FormValues = {
   username: string;
   email: string;
   channel: string;
+  socials: {
+    twitter: string;
+    facebook: string;
+  };
+  phoneNumbers: string[];
+  phNumbers: {
+    number: string;
+    isPrimary: boolean;
+  }[];
+  age: number;
+  dob: Date;
 };
 
 const YouTubeForm = () => {
   const form = useForm<FormValues>({
-    defaultValues: async () => {
-      const response = await fetch(
-        "https://jsonplaceholder.typicode.com/users/1"
-      );
-      const data = await response.json();
-      return {
-        username: data.name,
-        email: data.email,
-        channel: "Traversy Media",
-      };
+    defaultValues: {
+      username: "",
+      email: "",
+      channel: "",
+      socials: {
+        twitter: "",
+        facebook: "",
+      },
+      phoneNumbers: ["", ""],
+      phNumbers: [{ number: "", isPrimary: false }],
+      age: 0,
+      dob: new Date(),
     },
   });
 
@@ -28,6 +41,11 @@ const YouTubeForm = () => {
 
   const { register, control, handleSubmit, formState } = form;
   const { errors } = formState;
+
+  const { fields, append, remove } = useFieldArray({
+    name: "phNumbers",
+    control,
+  });
 
   return (
     <div>
@@ -81,6 +99,122 @@ const YouTubeForm = () => {
             {...register("channel", { required: "Channel is required" })}
           />
           <p className="error">{errors.channel?.message}</p>
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="twitter">Twitter</label>
+          <input
+            type="text"
+            id="twitter"
+            {...register("socials.twitter", {
+              required: "Twitter handle is required",
+            })}
+          />
+          <p className="error">{errors.socials?.twitter?.message}</p>
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="facebook">facebook</label>
+          <input
+            type="text"
+            id="facebook"
+            {...register("socials.facebook", {
+              required: "Facebook handle is required",
+            })}
+          />
+          <p className="error">{errors.socials?.facebook?.message}</p>
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="primary-phone">Primary Phone Number</label>
+          <input
+            type="text"
+            id="primary-phone"
+            {...register("phoneNumbers.0", {
+              required: "Primary phone number is required",
+            })}
+          />
+          <p className="error">{errors.phoneNumbers?.[0]?.message}</p>
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="secondary-phone">SecondaryPhone Number</label>
+          <input
+            type="text"
+            id="secondary-phone"
+            {...register("phoneNumbers.1", {
+              required: "Secondary phone number is required",
+            })}
+          />
+          <p className="error">{errors.phoneNumbers?.[1]?.message}</p>
+        </div>
+
+        <div>
+          <label htmlFor="">List of phone numbers</label>
+          <div>
+            {fields.map((field, index) => {
+              return (
+                <div className="form-control" key={field.id}>
+                  <input
+                    type="text"
+                    {...register(`phNumbers.${index}.number`)}
+                  />
+                  <div style={{ display: "flex" }}>
+                    <input
+                      type="checkbox"
+                      {...register(`phNumbers.${index}.isPrimary`)}
+                      id={`phNumbers.${index}.isPrimary`}
+                    />
+                    <label htmlFor={`phNumbers.${index}.isPrimary`}>
+                      Primary
+                    </label>
+                  </div>
+                  {index > 0 && (
+                    <button
+                      type="button"
+                      onClick={() => {
+                        remove(index);
+                      }}
+                    >
+                      Remove
+                    </button>
+                  )}
+                </div>
+              );
+            })}
+            <button
+              type="button"
+              onClick={() => append({ number: "", isPrimary: false })}
+            >
+              Add phone Number
+            </button>
+          </div>
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="age">Age</label>
+          <input
+            type="number"
+            id="age"
+            {...register("age", {
+              valueAsNumber: true,
+              required: "Age is required",
+            })}
+          />
+          <p className="error">{errors.age?.message}</p>
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="dob">Date of Birth</label>
+          <input
+            type="date"
+            id="dob"
+            {...register("dob", {
+              valueAsDate: true,
+              required: "Dob is required",
+            })}
+          />
+          <p className="error">{errors.dob?.message}</p>
         </div>
 
         <button type="submit">Submit</button>
