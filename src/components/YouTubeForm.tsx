eedcd5c03af7110fb6cd275b41dbc3e80d4569eb
Md Abu Zafar <mdabuzafar78@gmail.com>
2024-04-05 05:46:58 +0530

    26 - Learn about async validation

--- src/components/YouTubeForm.tsx
+++ src/components/YouTubeForm.tsx
@@ -145,6 +145,13 @@ const YouTubeForm = () => {
                     "This domain is not supported"
                   );
                 },
+                emailAvailable: async (fieldValue) => {
+                  const response = await fetch(
+                    `https://jsonplaceholder.typicode.com/users?email=${fieldValue}`
+                  );
+                  const data = await response.json();
+                  return data.length === 0 || "Email is already taken";
+                },
               },
             })}
           />
